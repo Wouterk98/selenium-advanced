78d2adfda34808b0a28047366b9f32c7306266cb
Wouterk98 <wouter.keultjes@gmail.com>
2023-04-21 11:42:00 +0200

    Clean up

--- src/test/java/com/caqart/todo/testcases/TodoTest.java
+++ src/test/java/com/caqart/todo/testcases/TodoTest.java
@@ -2,13 +2,9 @@ package com.caqart.todo.testcases;
 
 import com.caqart.todo.base.BaseTest;
 import com.caqart.todo.pages.SignupPage;
-import com.caqart.todo.pages.TodosPage;
 import com.github.javafaker.Faker;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static java.awt.SystemColor.text;
 
 public class TodoTest extends BaseTest {
 
@@ -28,20 +24,6 @@ public class TodoTest extends BaseTest {
                 .addTodo(todoItem)
                .isTodoDisplayed();
         Assert.assertTrue(isTodoDisplayed);
-
-//        driver.get("https://todo.qacart.com/signup");
-//        driver.findElement(By.cssSelector("[data-testid=first-name]")).sendKeys(firstName);
-//        driver.findElement(By.cssSelector("[data-testid=last-name]")).sendKeys(lastName);
-//        driver.findElement(By.cssSelector("[data-testid=email]")).sendKeys(email);
-//        driver.findElement(By.cssSelector("[data-testid=password]")).sendKeys(password);
-//        driver.findElement(By.cssSelector("[data-testid=confirm-password]")).sendKeys(password);
-//        driver.findElement(By.cssSelector("[data-testid=submit")).click();
-//        driver.findElement(By.cssSelector("[data-testid=add")).click();
-//        driver.findElement(By.cssSelector("[data-testid=new-todo")).sendKeys(todoItem);
-//        driver.findElement(By.cssSelector("[data-testid=submit-newTask")).click();
-//        String text = driver.findElement(By.cssSelector("[data-testid=todo-item")).getText();
-//        Assert.assertEquals(text, todoItem);
-//        Thread.sleep(1000);
     }
 
     @Test
@@ -61,20 +43,5 @@ public class TodoTest extends BaseTest {
                 .deleteTodo()
                 .noTodosDisplayed();
         Assert.assertTrue(noTodosDisplayed);
-
-//        driver.get("https://todo.qacart.com/signup");
-//        driver.findElement(By.cssSelector("[data-testid=first-name]")).sendKeys(firstName);
-//        driver.findElement(By.cssSelector("[data-testid=last-name]")).sendKeys(lastName);
-//        driver.findElement(By.cssSelector("[data-testid=email]")).sendKeys(email);
-//        driver.findElement(By.cssSelector("[data-testid=password]")).sendKeys(password);
-//        driver.findElement(By.cssSelector("[data-testid=confirm-password]")).sendKeys(password);
-//        driver.findElement(By.cssSelector("[data-testid=submit")).click();
-//        driver.findElement(By.cssSelector("[data-testid=add")).click();
-//        driver.findElement(By.cssSelector("[data-testid=new-todo")).sendKeys(todoItem);
-//        driver.findElement(By.cssSelector("[data-testid=submit-newTask")).click();
-//        driver.findElement(By.cssSelector("[data-testid=delete")).click();
-//        boolean isNoTodoDisplayed = driver.findElement(By.cssSelector("[data-testid=no-todos")).isDisplayed();
-//        Assert.assertTrue(isNoTodoDisplayed);
-//        Thread.sleep(1000);
     }
 }
